bbd6fb3d3b9ee1ee6320e8f98ebb6a5bfb7fbb6b
osrsbox <phoil@osrsbox.com>
2018-12-29 14:43:48 +1300

    Created two folders for extraction tools - one for wiki, one for other. This was becuase the folders were getting messy. Added buy limits, name normalization and skill requirement material to extraction_tools_other folder.

---
+++ extraction_tools_other/README.md
@@ -0,0 +1,34 @@
+# osrsbox-db: OSRS Other data extraction tools
+
+## all_buy_limits.py
+
+- A simple script to parse the `ge_limits.json` file from the RuneLite client, compare then against the osrsbox-db. The goal is to convert the item ID to name, and extract the buy limit data with it.
+- Run using:
+- `python3.6 all_buy_limits.py -i ../docs/items-json/`
+- `python.exe all_buy_limits.py -i ..\docs\items-json\`
+
+## all_buy_limits.txt
+
+- This document contains a list of item name to buy limits.
+- For example:
+- `Cannonball|7000`
+
+## ge_limits.json
+
+- This document is from the RuneLite client and contains a list of buy limits
+- [Source](https://github.com/runelite/runelite/blob/master/runelite-client/src/main/resources/net/runelite/client/plugins/grandexchange/ge_limits.json)
+
+## item_skill_requirements.json
+
+- This document contains JSON formatted data that maps all equipable item IDs to an array of skill requirements. An example structure is seen below:
+- `"1061": [{"skill": "defence", "level": 1}]`
+
+## normalized_names.txt
+
+- This document stores information about OSRS items that are not directly extractable from the OSRS Wiki. The purpose of the document is to aid item metadata extraction by normalizing item names
+- The structure of the file is:
+    - `itemID|itemName|normalizedName|statusCode`
+    - `itemID` is directly from the OSRS client cache
+    - `itemName` is directly from the OSRS client cache
+    - `nomalizedName` is the itemName normalized for the OSRS Wiki
+    - `statusCode` documents specific item scenarios
---
+++ extraction_tools_other/all_buy_limits.py
@@ -0,0 +1,80 @@
+# !/usr/bin/python
+
+"""
+Author:  PH01L
+Email:   phoil@osrsbox.com
+Website: osrsbox.com
+Date:    2018/12/29
+
+Description:
+Simple caller script to print item names
+
+Copyright (c) 2018, PH01L
+
+###############################################################################
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+>>> CHANGELOG:
+    1.0.0       Base functionality
+"""
+
+__version__ = "1.0.0"
+
+import os
+import sys
+import json
+
+# Import osrsbox-db API classes
+path = ".." + os.sep + "item_api_tools" + os.sep
+sys.path.append(path)
+import AllItems
+import ItemDefinition
+import ItemBonuses
+
+################################################################################
+if __name__=="__main__":
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", 
+                    "--input", 
+                    required=True,
+                    help="Two options: 1) Directory of JSON item files (../docs/items-json), or 2) Single JSON file (../docs/items_complete.json) ")
+    args = vars(ap.parse_args())
+    
+    ge_limits = dict()
+    # Load the ge_limits.json file
+    with open('ge_limits.json') as f:
+        ge_limits = json.load(f)
+
+    # Start processing    
+    ai = AllItems.AllItems(args["input"])
+
+    # Get a dict with: id -> ItemDefinition
+    all_item_ids = dict()
+    for item in ai:
+        all_item_ids[item.id] = item
+    
+    # Make a list of: name|buy_limit
+    buy_limits_list = list()
+    for entry in ge_limits:
+        known_item_id = int(entry)
+        buy_limit = ge_limits[entry]
+        line = "%s|%d" % (all_item_ids[known_item_id].name, buy_limit)
+        buy_limits_list.append(line)
+
+    # Write out buy limit list
+    f = open("all_buy_limits.txt", "w")
+    for line in buy_limits_list:
+        f.write(line)
+        f.write("\n")
+    f.close()
