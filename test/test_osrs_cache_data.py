26485eac87dcbafd958f206211a875802dfda1ef
osrsbox <phoil@osrsbox.com>
2019-03-01 14:52:14 +1300

    Updated data for this weeks in-game update. Database re-gen to follow.

--- test/test_osrs_cache_data.py
+++ test/test_osrs_cache_data.py
@@ -40,7 +40,7 @@ def test_osrs_cache_data_compression(test_data, expected):
 @pytest.mark.parametrize("cache_type,expected", [
     ("items", 23110),
     ("npcs", 8645),
-    ("objects", 34705)
+    ("objects", 34713)
 ])
 def test_osrs_cache_data_decompression(path_to_cache_dir: Path, cache_type, expected):
     path_to_cache_file = path_to_cache_dir / f"{cache_type}.json"
