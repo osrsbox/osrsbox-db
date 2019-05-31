a2d1e3aee7e5fae7598975d1221c57716da1fb5c
osrsbox <phoil@osrsbox.com>
2019-06-01 07:43:12 +1200

    Weekly cache and wiki data update.

--- test/test_osrs_cache_data.py
+++ test/test_osrs_cache_data.py
@@ -38,7 +38,7 @@ def test_osrs_cache_data_compression(test_data, expected):
 
 
 @pytest.mark.parametrize("cache_type,expected", [
-    ("items", 23458),
+    ("items", 23487),
     ("npcs", 8692),
     ("objects", 34826)
 ])
