c99d3d4478345c1d6c86bb6da569a67915563511
osrsbox <phoil@osrsbox.com>
2020-10-24 09:53:56 +1300

    Updated python package. Reworded update script to use relative paths, and new cache data.

--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -44,47 +44,74 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 '
+# Setup commonly used directories
+odb=$(cd ..; pwd)
+rl=$(cd ..; cd ..; cd runelite; pwd)
+
+# Update all submodules
+cd $odb
+git submodule update --recursive --remote
+
+
+# CACHE
+# Build osrs-flatcache
+echo -e ">>> Building osrs-cache..."
+cd $odb/data/cache/osrs-flatcache
+mvn clean
+mvn install -Dcheckstyle.skip=false
+
+# Find the packer.jar file with current version and bundled with dependencies
+# For example: packer-1.6.10-shaded.jar
+cd packer/target
+jar_file=$(ls | grep .shaded.jar)
+
+# Extract the flat cache
+rm -rf $odb/data/cache/cache-data
+mkdir -p $odb/data/cache/cache-data
+java -jar $jar_file unpack $odb/data/cache/osrs-cache $odb/data/cache/cache-data
+
 
 # RUNELITE
 # Update RuneLite
 echo -e ">>> Updating RuneLite..."
-cd ~/repos/runelite
+cd $rl
 git pull
 
-echo -e ">>> Building RuneLite..."
 # Build using mvn command
+echo -e ">>> Building RuneLite..."
 mvn clean
 mvn install -DskipTests
 
 # Find the cache.jar file with current version and bundled with dependencies
 # For example: cache-1.5.27-SNAPSHOT-jar-with-dependencies.jar
-cd ~/repos/runelite/cache/target
+cd $rl/cache/target
 jar_file=$(ls | grep .jar-with-dependencies.)
 
 # Remove old cache dump
 echo -e ">>> Removing the old cache dump in osrsbox-db..."
-rm -r ~/repos/osrsbox-db/data/cache/items/
-rm -r ~/repos/osrsbox-db/data/cache/npcs/
-rm -r ~/repos/osrsbox-db/data/cache/objects/
+rm -r $odb/data/cache/items/
+rm -r $odb/data/cache/npcs/
+rm -r $odb/data/cache/objects/
 
 # Dump the cache
 echo -e ">>> Dumping cache using RuneLite cache tool..."
 java -classpath $jar_file net.runelite.cache.Cache \
--cache ~/jagexcache/oldschool/LIVE \
--items ~/repos/osrsbox-db/data/cache/items
+-cache $odb/data/cache/cache-data \
+-items $odb/data/cache/items
 
 java -classpath $jar_file net.runelite.cache.Cache \
--cache ~/jagexcache/oldschool/LIVE \
--npcs ~/repos/osrsbox-db/data/cache/npcs
+-cache $odb/data/cache/cache-data \
+-npcs $odb/data/cache/npcs
 
 java -classpath $jar_file net.runelite.cache.Cache \
--cache ~/jagexcache/oldschool/LIVE \
--objects ~/repos/osrsbox-db/data/cache/objects
+-cache $odb/data/cache/cache-data \
+-objects $odb/data/cache/objects
+
 
 # OSRSBOX
 # Update osrsbox-db
 echo -e ">>> Updating osrsbox-db..."
-cd ~/repos/osrsbox-db
+cd $odb
 git pull
 
 # Create virtual environment and activate
@@ -99,38 +126,38 @@ lastUpdate=$(date +%Y-%m-%dT%H:%M:%SZ -d "$(stat -c %x data/wiki/page-titles-ite
 
 # Update wiki data for items/monsters
 echo -e ">>> Updating wiki data..."
-cd ~/repos/osrsbox-db/scripts/update/
+cd $odb/scripts/update/
 python3 update_wiki_data.py $lastUpdate
 
 # Generate the processed wikitext files
 echo -e ">>> Process raw wikitext..."
-cd ~/repos/osrsbox-db/scripts/wiki/
+cd $odb/scripts/wiki/
 python3 process_wikitext.py
 
 # Update items unalchable file
 echo -e ">>> Update unalchable items..."
-cd ~/repos/osrsbox-db/scripts/items/
+cd $odb/scripts/items/
 python3 generate_items_unalchable.py
 
 # Update local cache data from cache dump
 echo -e ">>> Updating cache data..."
-cd ~/repos/osrsbox-db/scripts/update/
+cd $odb/scripts/update/
 python3 update_cache_data.py
 
 # Determine cache changes
 echo -e ">>> Determine any cache changes..."
-cd ~/repos/osrsbox-db/scripts/update/
+cd $odb/scripts/update/
 python3 determine_cache_changes.py
 
 # Move the new cache data
-cd ~/repos/osrsbox-db/
+cd $odb/
 mv data/cache/items-cache-data.json data/items/
 mv data/cache/monsters-cache-data.json data/monsters/
 
 # Perform pre update_db.sh check for...
 # item skill requirements, item weapon types, 
 # item buy limits, and item icons
-cd ~/repos/osrsbox-db/scripts/items/
+cd $odb/scripts/items/
 echo -e ">>> Items with no skill requirements..."
 python3 check_item_skill_requirements.py
 echo -e ">>> Items with no weapon types..."
@@ -138,7 +165,7 @@ python3 check_item_weapon_types.py
 echo -e ">>> Items with no buy limits..."
 python3 check_item_buy_limits.py
 
-cd ~/repos/osrsbox-db/scripts/icons/
+cd $odb/scripts/icons/
 echo -e ">>> Items with no icon..."
 python3 check_item_icons.py
 
